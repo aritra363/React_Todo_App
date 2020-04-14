ed978e80d0b44f7a0f29f25ac3fa1ca80c87abbc
aritra363 <aritrapaul363@gmail.com>
2020-04-14 18:04:16 +0530

    Updates

---
+++ precache-manifest.f44e224d9553b31cc23be150fc2aac91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d52868c7d7d7ab6da77949cf14e00c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b132c871d06f1038370",
+    "url": "/static/css/main.10c5ee5a.chunk.css"
+  },
+  {
+    "revision": "c9d1838a961e5e3eb8b3",
+    "url": "/static/js/2.143243e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.143243e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b132c871d06f1038370",
+    "url": "/static/js/main.37844c40.chunk.js"
+  },
+  {
+    "revision": "bcc5b5410d29660f6648",
+    "url": "/static/js/runtime-main.d3a82e94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
